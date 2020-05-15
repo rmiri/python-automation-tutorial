06603e9c3ae6d4d34c463d6a1b6e81320b430dc6
github username <r.miriuk@tutanota.com>
2020-05-16 00:02:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-automation-tutorial
+
+Tutorial from Linkedin Learning
---
+++ quoteScraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://quotes.toscrape.com/'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'lxml')
+
+quotes = soup.find_all('span', class_='text')
+authors = soup.find_all('small', class_='author')
+
+for i in range(0, len(quotes)):
+    print(quotes[i].text)
+    print(authors[i].text)
+
+
+
